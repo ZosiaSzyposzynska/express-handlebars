c1520e67f78ee0e097f73178897ff8352a12b370
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-12-11 19:20:13 +0100

    Add changes for contact server.js

--- server.js
+++ server.js
@@ -1,41 +1,58 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage: storage });
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about')
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.render('contact')
+  res.render('contact');
 });
 
 app.get('/info', (req, res) => {
-  res.render('info')
+  res.render('info');
 });
 
 app.get('/history', (req, res, next) => {
-  res.render('history')
+  res.render('history');
 });
 
 app.get('/hello/:name', (req, res) => {
   res.render('hello', { layout: false, name: req.params.name });
 });
 
+app.post('/contact/send-message', upload.single('fileName'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    const fileName = req.file.originalname;
+    res.render('contact', { isSent: true, fileName: fileName });
+  } else {
+    res.render('contact', { isError: true });
+  }
+});
+
 app.use((req, res) => {
   res.status(404).send('404 not found...');
-})
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
